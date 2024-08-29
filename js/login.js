a0f428a5af58e28a797685eb65a3982bbadf9166
Chipili12 <santibur@live.com>
2024-08-29 18:01:11 -0300

    Added email in Navbar instead of 'Login' when logged in, using the init.js file. Added init.js to login.html.

--- js/login.js
+++ js/login.js
@@ -1,8 +1,9 @@
-let login = document.getElementById("login");  // Tomo los datos del login
+let login = document.getElementById("login"); // Tomo los datos del login
 
-if (login) { 
+if (login) {
   login.addEventListener("submit", () => { // evento que se activa cuando envia el formulario
     sessionStorage.setItem("isAuthenticated", "true"); // cuando el usuario envia el formulario, lo marca como autenticado
+    sessionStorage.setItem("email", document.getElementById("email").value);
     window.location.href = "index.html"; // Redirigir al index si se autentica
   });
 }
