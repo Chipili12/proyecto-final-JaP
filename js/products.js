e9bfe141c9dbb2eeded50050ae3e1ac5fd4d9d7f
floseq <88863373+floseq@users.noreply.github.com>
2024-09-10 11:10:18 -0300

    orden y filtrado de products

--- js/products.js
+++ js/products.js
@@ -7,21 +7,21 @@ let categoryData = undefined;
 
 // Declaro la función que obtiene los datos de una categoría de productos realizando un fetch a la API
 const fetchProduct = async () => {
-    try {
-        const respuesta = await fetch(API_URL);
-        return await respuesta.json();
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const respuesta = await fetch(API_URL);
+    return await respuesta.json();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // Declaro la función para mostrar la lista de productos en el DOM, utilizando el JSON dado por la API
-const showProductsList = () => {
-    let htmlContentToAppend = "";
-    for (let i = 0; i < categoryData.products.length; i++) {
-        let product = categoryData.products[i];
-        {
-            htmlContentToAppend += `
+const showProductsList = (array) => {
+  let htmlContentToAppend = "";
+  for (let i = 0; i < array.length; i++) {
+    let product = array[i];
+    {
+      htmlContentToAppend += `
             <div class="list-group-item list-group-item-action cursor-active py-3">
                 <div class="row">
                     <div class="col-3">
@@ -40,15 +40,63 @@ const showProductsList = () => {
                 </div>
             </div>
             `;
-        }
-        document.getElementById("products").innerHTML = htmlContentToAppend;
     }
+    document.getElementById("products").innerHTML = htmlContentToAppend;
+  }
 };
-
-
 
 // Llamo la función para realizar el fetch, posteriormente asigno el resultado en formato JSON a la variable categoryData, y llamo a la función showProductsList.
 fetchProduct().then((data) => {
-    categoryData = data;
-    showProductsList();
+  categoryData = data.products;
+  showProductsList(categoryData);
 });
+
+// Filtrar y ordenar productos
+document.addEventListener("DOMContentLoaded", function (e) {});
+
+document.getElementById("sortByPrice").addEventListener("click", function () {
+  const filteredCategoryData = datosFiltrado();
+
+  let resultPrice = []; // arreglo vacio
+  let orden = document.getElementById("UpDown").className;
+
+  if (orden == "fas fa-sort-amount-up mr-1") {
+    // De menor a mayor
+    resultPrice = filteredCategoryData.sort(function (a, b) {
+      if (a.cost < b.cost) {
+        return -1;
+      }
+      if (a.cost > b.cost) {
+        return 1;
+      }
+      return 0;
+    });
+    document.getElementById("UpDown").className = "fas fa-sort-amount-down mr-1";
+  } else {
+    // De mayor a menor
+    resultPrice = filteredCategoryData.sort(function (a, b) {
+      if (a.cost > b.cost) {
+        return -1;
+      }
+      if (a.cost < b.cost) {
+        return 1;
+      }
+      return 0;
+    });
+    document.getElementById("UpDown").className = "fas fa-sort-amount-down mr-1";
+  }
+  showProductsList(resultPrice);
+});
+
+document.getElementById("rangeFilterCount").addEventListener("click", () => {
+  showProductsList(datosFiltrado());
+});
+
+function datosFiltrado() {
+  let minimo = parseInt(document.getElementById("rangeFilterCountMin").value);
+  let maximo = parseInt(document.getElementById("rangeFilterCountMax").value);
+  minimo = isNaN(minimo) ? 0 : minimo; // Si es NaN, se asigna 0
+  maximo = isNaN(maximo) ? Number.MAX_VALUE : maximo; // Si es NaN, se asigna un valor muy alto
+  const filteredCategoryData = categoryData.filter((item) => item.cost > minimo && item.cost < maximo);
+  return filteredCategoryData;
+}
